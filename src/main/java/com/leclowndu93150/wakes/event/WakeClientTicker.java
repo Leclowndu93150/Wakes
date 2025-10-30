f777bf0d31f14f5b69b76d65179e8dd29fccb31c
Leclowndu93150 <xdr.a123454321@gmail.com>
2025-10-30 11:27:19 +0100

    fix https://github.com/Leclowndu93150/Wakes/issues/8

--- src/main/java/com/leclowndu93150/wakes/event/WakeClientTicker.java
+++ src/main/java/com/leclowndu93150/wakes/event/WakeClientTicker.java
@@ -2,9 +2,11 @@ package com.leclowndu93150.wakes.event;
 
 import com.leclowndu93150.wakes.simulation.WakeHandler;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.event.level.LevelEvent;
 
 @EventBusSubscriber(bus =  EventBusSubscriber.Bus.GAME)
 public class WakeClientTicker {
@@ -15,6 +17,13 @@ public class WakeClientTicker {
             WakeHandler.kill();
         } else if (WakeHandler.getInstance(client.level).isEmpty()) {
             WakeHandler.init(client.level);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onLevelUnload(LevelEvent.Unload event) {
+        if (event.getLevel() instanceof ClientLevel clientLevel) {
+            WakeHandler.killDimension(clientLevel.dimension());
         }
     }
 }
